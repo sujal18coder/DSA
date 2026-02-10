358e1335a2118aa8fc7650784566fbe7d212d308
sujal18coder <sujal18coder>
2026-02-10 17:31:36 +0530

    b1

---
+++ my basic program/b1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    if(n<0)
+    cout<<"negative";
+    else if(n==0)
+    cout<<"zero";
+    else
+    cout<<"positive";
+}
